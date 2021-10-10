77049c3aa6cd622bf25d81fd8cdedc771162e1f6
F_Dudley4 <50776815+F-Dudley@users.noreply.github.com>
2021-10-10 14:22:16 +0100

    Completed Basic Character Controller
    Create a Character Controller and Camera Controller so the player can fly round the scene as a camera then take control of certain characters.

--- code/AI For Games Project/Assets/Scripts/CameraController.cs
+++ code/AI For Games Project/Assets/Scripts/CameraController.cs
@@ -13,17 +13,21 @@ public class CameraController : MonoBehaviour
     /// <summary>
     /// The Rotation of the Camera or Selected Character.
     /// </summary>
-    public float rotationSpeed = 100f;
+    public float rotationSpeed = 10f;
     public float boostMultiplier = 10f;
 
     private float xEulerRotation, yEulerRotation;
 
     private Transform cameraTransform;
     private Transform cameraContainer;
+
+    [Header("Interaction Variables")]
+    public float interactionRange = 100f;
+
     [Header("Character Variables")]
     public bool controllingCharacter;
 
-    private CharacterController characterScript;
+    private CharacterMovement characterScript;
 
     private void Start()
     {
@@ -40,11 +44,12 @@ public class CameraController : MonoBehaviour
             default:
             case false:
                 UserInput();
+                CheckCameraRay();
                 UpdateCameraTransform();
                 break;
 
             case true:
-                Debug.Log("Controlling Character");
+                CharacterControl();
                 break;
         }
 
@@ -56,7 +61,7 @@ public class CameraController : MonoBehaviour
     private void UserInput() {
 
         if(Input.GetKeyDown(KeyCode.P)) {
-            // Toggle Between Character and Cam.
+            RemoveParent();
         }
 
         if(Input.GetKeyDown(KeyCode.L)) {
@@ -67,34 +72,94 @@ public class CameraController : MonoBehaviour
     private float GetMouseY() => Input.GetAxis("Mouse Y") * rotationSpeed;
     private float GetMouseX() => Input.GetAxis("Mouse X") * rotationSpeed;
 
-    private float GetMovementX() {
-        float speed = movementSpeed;
-        if(Input.GetKey(KeyCode.LeftShift)) speed *= boostMultiplier;
-        float horizontal =Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
-
-        return horizontal;
-    }
-
-    private float GetMovementY() {
-        float speed = movementSpeed;
-        if(Input.GetKey(KeyCode.LeftShift)) speed *= boostMultiplier;
-        float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-
-        return vertical;
-    }
+    private float GetMovementX() => Input.GetAxis("Horizontal") * Time.deltaTime;
+    private float GetMovementY() => Input.GetAxis("Vertical") * Time.deltaTime;
 
 #endregion
 
+#region Camera Functions
+
+    private void SetControl(CharacterMovement controller) {
+        if(controller != null) {
+            characterScript = controller;
+            controllingCharacter = true;
+            characterScript.currentlyControlled = true;
+        }
+        else {
+            controllingCharacter = false;
+            if(characterScript) {
+                characterScript.currentlyControlled = false;
+                characterScript = null;                
+            }
+        }
+    }
+
     private void UpdateCameraTransform() {
 
+        float speed = movementSpeed;
+        if(Input.GetKey(KeyCode.LeftShift)) speed *= boostMultiplier;
+
         // Rotation
+
         float newRotationX = transform.localEulerAngles.x - GetMouseY();
         float newRotationY = transform.localEulerAngles.y + GetMouseX();
 
         transform.localRotation = Quaternion.Euler(newRotationX, newRotationY, 0.0f);
 
         // Movement
-        transform.position += transform.forward * GetMovementY();
-        transform.position += transform.right * GetMovementX();
+        transform.position += transform.forward * (GetMovementY() * speed);
+        transform.position += transform.right * (GetMovementX() * speed);
     }
+
+    private void CheckCameraRay() {
+        RaycastHit hit;
+
+        bool castCollide = Physics.Raycast(transform.position, transform.forward, out hit, interactionRange, LayerMask.GetMask("Characters"));
+        if(castCollide) {
+            Debug.Log("Casting on Character");
+            CharacterMovement controller = hit.collider.transform.root.GetComponent<CharacterMovement>();
+
+            if(Input.GetKeyDown(KeyCode.F)) {
+                transform.SetParent(controller.cameraPosition);
+                LeanTween.move(this.gameObject, controller.cameraPosition, 1.0f).setEaseInCubic()
+                .setOnStart(() => {
+                    LeanTween.rotate(this.gameObject, controller.cameraPosition.forward, 1.0f).setEaseInCubic();
+                })
+                .setOnComplete(() => {
+                    if(controller.userControllable) {
+                        SetControl(controller);
+                    }
+                });
+
+            }
+        }
+
+        Debug.DrawRay(transform.position, transform.forward * interactionRange, castCollide ? Color.red : Color.green, 0.0f);
+    }
+
+    private void RemoveParent() {
+        if(transform.parent) {
+            transform.SetParent(null, true);
+
+            SetControl(null);
+        }
+    }
+
+#endregion
+
+#region Character Control Functions
+
+    private void CharacterControl() {
+        // Rotation
+        float newRotationX = transform.localEulerAngles.x - GetMouseY();
+
+        transform.localRotation = Quaternion.Euler(newRotationX, 0.0f, 0.0f);
+        characterScript.UpdateCharacterRotation(GetMouseX());
+
+        // Position
+        characterScript.UpdateCharacterPosition(GetMovementY(), GetMovementX());
+    }
+
+#endregion
+
 }
