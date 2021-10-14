cce3f3ef422893eaeb07cb26207fb1cba58a0155
F_Dudley4 <50776815+F-Dudley@users.noreply.github.com>
2021-10-14 23:55:15 +0100

    Added Stack-Based Finite State Machine Code
    Added Stack Based Finite State Machine Code, and refactored slightly for linting.

---
+++ code/AI For Games Project/Assets/Scripts/FSM/State.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State
+{
+    protected Agent owner;
+    protected StateManager stateManager;
+
+    public State(Agent owner, StateManager stateManager)
+    {
+        this.owner = owner;
+        this.stateManager = stateManager;
+    }
+
+    public abstract void Enter();
+    public abstract void Execute();
+    public abstract void Exit();
+}
---
+++ code/AI For Games Project/Assets/Scripts/FSM/States/IdleState.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : State
+{
+    public IdleState(Agent _owner, StateManager _stateManager) : base(_owner, _stateManager)
+    {
+        
+    }
+
+    public override void Enter()
+    {
+        Debug.Log("Entering Idle State");
+    }
+
+    public override void Execute()
+    {
+        Debug.Log("Executing Idle State");
+    }
+
+    public override void Exit()
+    {
+        Debug.Log("Exiting Idle State");
+    }
+}
