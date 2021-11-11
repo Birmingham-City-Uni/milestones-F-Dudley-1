cccb08c84fd1501c97e8302c5b0e1d7d90ab253e
F-Dudley <50776815+F-Dudley@users.noreply.github.com>
2021-11-11 21:54:44 +0000

    Dev To Main: Game Terrain and Pathfinding (#16)
    * Camera Controller Completed (#5)
    
    * Added New Camera Controller, and Started Character Controller
    
    Added a Free Camera Controller to Go Around the Scene, and Also Started a Camera Controller for the player to take control of in future.
    
    * Completed Basic Character Controller
    
    Create a Character Controller and Camera Controller so the player can fly round the scene as a camera then take control of certain characters.
    
    * New GitIgnore as GitIgnore In Unity Project Not Being Used
    
    * AI Sensor (#7)
    
    * Added Sensors Script
    
    Added Sensors Script From Labs
    
    Needs Raybundle Needs Refining Slightly
    
    * Refactored Sensors.cs
    
    Cleaned-Up Sensors Script and Sectioned Appropriately
    
    * Refactored Camera Controller For Lint
    
    * Refactoring For Lint In CharacterMovement Script
    
    * Linting Scripts Again
    
    * Code Linting
    
    * Final Lint Hopefully
    
    * ... Lint
    
    * Added Stack-Based Finite State Machine Code (#9)
    
    Added Stack Based Finite State Machine Code, and refactored slightly for linting.
    
    * Linting Changes
    
    * A* Pathfinding System (#13)
    
    * Node Grid Implementation w/ Sizing and Offset Capabilities
    
    Implemented a Node Grid with Object Detection and Interfacing Capabilities.
    
    * Implemented A* Pathfinder
    
    * Implementation of a Heap/Priority Queue For Pathfinding
    
    * Created a PathFinding Manager To Request And Distribute Agent Paths
    
    * Started Implementation of Pathfinding System in Main Scene
    
    * Game world (#15)
    
    * Created Basic Terrain with Texturing
    
    Created a Terrain with a Texturing and a layout for the scene.
    
    * Finished Terrain
    
    Finished Terrain Using Unity Terrain System, which includes Trees Placed Around.
    Added The Option For Future Grass Painting with the Grass Brush.
    
    * Project Settings Change
    
    * Changed SuperLinter Requirements
    
    * Update linter.yml
    
    Updated Linter For Bug Workaround
    
    * Code Linting Changes
    
    Changes Required in the Code Linter
    
    * DataItem Lint

---
+++ code/AI For Games Project/Assets/Scripts/Path Finding/Node.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : IPQueueItem<Node>
+{
+    [Header("World Variables")]
+    public bool isWalkable;
+    public Vector3 worldPosition;
+    public int gridX, gridZ;
+
+    [Header("Path Finding Costs")]
+    public Node parentNode;
+    public int gCost;
+    public int hCost;
+
+    private int queueIndex;
+
+    public Node(bool _isWalkable, Vector3 _worldPosition, int _gridX, int _gridZ)
+    {
+        isWalkable = _isWalkable;
+        worldPosition = _worldPosition;
+        gridX = _gridX;
+        gridZ = _gridZ;
+    }
+
+    public int fCost
+    {
+        get
+        {
+            return gCost + hCost;
+        }
+    }
+
+    public int QueueIndex
+    {
+        get
+        {
+            return queueIndex;
+        }
+        set
+        {
+            queueIndex = value;
+        }
+    }
+
+    public int CompareTo(Node _NodeToCompare)
+    {
+        int compare = fCost.CompareTo(_NodeToCompare.fCost);
+        if (compare == 0)
+        {
+            compare = hCost.CompareTo(_NodeToCompare.hCost);
+        }
+
+        return -compare;
+    }
+}
