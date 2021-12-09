bb38ae003a5803f23e85811d17e1bd27de485d66
F_Dudley4 <50776815+F-Dudley@users.noreply.github.com>
2021-12-09 14:10:14 +0000

    Boid Scripts Commit

---
+++ code/AI For Games Project/Assets/Scripts/Boids/FlockAgent.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockAgent : MonoBehaviour
+{
+    [Header("Bird Attributes")]
+    public float movementSpeed = 1f;
+    public float maxMovementSpeed = 5f;
+    public float minMovementSpeed = 1f;
+
+    public float rotationSpeed = 3f;
+    public bool turning = true;
+
+    [Space]
+
+    public float neighbourRange = 10f;
+
+    [Header("Steering Behaviours")]
+    public Vector3 lastPlayerPos;
+    public float seperationRange = 6f;
+
+
+    [Header("Enviroment References")]
+    [SerializeField] private Transform player;
+
+    public void SetPlayer(Transform _player) => player = _player;
+
+    #region Unity Functions
+    private void Start()
+    {
+        movementSpeed = Random.Range(minMovementSpeed, maxMovementSpeed);
+        lastPlayerPos = player.position;
+    }
+
+    private void Update()
+    {
+        if (!FlockManager.boundsCollider.bounds.Contains(transform.position))
+        {
+            turning = true;
+        }
+        else if (transform.position.y <= 10f)
+        {
+            turning = true;
+        }
+        else turning = false;
+        
+        if (turning)
+        {
+            Vector3 direction = (player.position + FlockManager.instance.GetPositionInBounds()) - transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                  Quaternion.LookRotation(direction),
+                                                  rotationSpeed * Time.deltaTime);
+        }
+
+        if (Random.Range(0, 5) < 1)
+        {
+            Vector3 boidCohesion = transform.position;            
+            Vector3 boidSeperation = lastPlayerPos;
+            Vector3 boidAlignment;
+
+            int neighbourAmount = 0;
+            float neighbourDistance;
+            float neighbourSpeed = 0.5f;
+
+            foreach (GameObject bird in FlockManager.instance.birds)
+            {
+                if (bird != this.gameObject)
+                {
+                    neighbourDistance = Vector3.Distance(bird.transform.position, this.transform.position);
+                    if (neighbourDistance <= neighbourRange)
+                    {
+                        neighbourAmount++;
+                        boidCohesion += bird.transform.position;
+
+                        if (neighbourDistance < seperationRange)
+                        {
+                            boidSeperation = Seperation(boidSeperation, bird.transform);                        
+                        }
+
+                        neighbourSpeed += bird.GetComponent<FlockAgent>().movementSpeed;
+                    }                    
+                }
+            }
+
+            if (neighbourAmount >= 0)
+            {
+                boidCohesion = Cohesion(boidCohesion, neighbourAmount);
+                movementSpeed = (neighbourSpeed / neighbourAmount) + Random.Range(-0.1f, 0.1f);
+                if (movementSpeed > maxMovementSpeed) movementSpeed = Random.Range(minMovementSpeed, maxMovementSpeed);
+
+                boidAlignment = Alignment(boidCohesion, boidSeperation);
+
+                transform.rotation = Quaternion.Slerp(transform.rotation, 
+                                                      Quaternion.LookRotation(boidAlignment),
+                                                      rotationSpeed * Time.deltaTime);
+            }
+        }
+        transform.Translate(0, 0, Time.deltaTime * movementSpeed);
+    }
+    #endregion
+
+    #region Steering Behaviors
+
+    private Vector3 Cohesion(Vector3 _currentCohesionVector, int _neighbourAmount)
+    {
+        return _currentCohesionVector / _neighbourAmount + (FlockManager.targetPosition - transform.position);
+    }
+
+    private Vector3 Seperation(Vector3 _currentSeperationVector, Transform _neighbourBird)
+    {
+        return _currentSeperationVector + (transform.position - _neighbourBird.position);
+    }
+
+    private Vector3 Alignment(Vector3 _cohesionVector, Vector3 _seperationVector)
+    {
+        return (_cohesionVector + _seperationVector) - transform.position;
+    }
+
+    #endregion
+}
---
+++ code/AI For Games Project/Assets/Scripts/Boids/FlockManager.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class FlockManager : MonoBehaviour
+{
+    public static FlockManager instance;
+
+    [Header("Main Variables")]
+    public GameObject birdPrefab;
+    public Transform player;
+    public Transform boidsContainer;
+
+    public static BoxCollider boundsCollider;
+
+    [Header("Flock Attributes")]
+    public Vector3 flockBounds = new Vector3(0, 0, 0);
+    public float distanceFromGround = 10f;
+    public float targetChangeTime = 30f;
+
+    [SerializeField] public int numBirds = 100;
+    [SerializeField] public static Vector3 targetPosition = new Vector3(0f, 0f, 0f); 
+
+    public GameObject[] birds;
+    public Coroutine targetChange;
+
+    [Header("Debug")]
+    public bool drawFlockBounds;
+    public bool drawTargetGizmo;
+
+    #region Unity Functions
+    private void Awake()
+    {
+        instance = this;
+
+        targetPosition = GetPositionInBounds();
+        boundsCollider = GetComponent<BoxCollider>();
+
+        boundsCollider.center = new Vector3(0, distanceFromGround + (flockBounds.y / 2), 0);
+        boundsCollider.size = flockBounds;
+
+        birds = new GameObject[numBirds];
+        for (int i = 0; i < numBirds; i++)
+        {
+            GameObject birdInstance = Instantiate(birdPrefab, GetPositionInBounds(), Quaternion.LookRotation(new Vector3(Random.Range(-1,1), 0, Random.Range(-1, 1))));
+            birdInstance.GetComponent<FlockAgent>().SetPlayer(player);
+            birdInstance.transform.SetParent(boidsContainer);
+            birds[i] = birdInstance;
+        }
+
+        targetChange = StartCoroutine("ChangeTarget");
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (drawFlockBounds)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireCube(new Vector3(0, distanceFromGround + (flockBounds.y / 2), 0), flockBounds);
+        }
+
+        if (drawTargetGizmo)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(targetPosition, 4f);
+        }
+    }
+    #endregion
+
+    #region Positioning Functions
+    private IEnumerator ChangeTarget()
+    {
+        yield return new WaitForSeconds(targetChangeTime);
+        targetPosition = GetPositionInBounds();
+    }
+
+    public Vector3 GetPositionInBounds()
+    {
+        Vector3 halfBounds = flockBounds * 0.3f;
+
+        return new Vector3(Random.Range(transform.position.x - halfBounds.x, transform.position.x + halfBounds.x),
+                                     Random.Range(transform.position.y + distanceFromGround, transform.position.y + flockBounds.y),
+                                     Random.Range(transform.position.z - halfBounds.z, transform.position.z + halfBounds.z));
+    }
+
+    public static bool CheckInBounds(Vector3 _positionToCheck)
+    {
+        return boundsCollider.bounds.Contains(_positionToCheck);
+    }
+    #endregion
+}
