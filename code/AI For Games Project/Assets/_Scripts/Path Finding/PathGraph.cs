121aff9c1c385753bbaaae922fb392be039ffb90
F_Dudley4 <50776815+F-Dudley@users.noreply.github.com>
2022-01-04 22:35:27 +0000

    Revert "Revert "Merge branch 'main' into dev""
    This reverts commit 81e5e3b7638f9ed878ab0ca644cd32c4dbf57be9.

--- code/AI For Games Project/Assets/_Scripts/Path Finding/PathGraph.cs
+++ code/AI For Games Project/Assets/_Scripts/Path Finding/PathGraph.cs
@@ -6,61 +6,35 @@ using UnityEngine;
 public class PathGraph : MonoBehaviour, NodeContainer
 {
     [Header("Graph Attributes")]
-    /// <summary>
-    /// The Container To Check For Placed Node Objects.
-    /// </summary>
     public Transform graphNodeContainer;
 
     [Space]
 
-    /// <summary>
-    /// The Max Distance To Check For Neighbouring Nodes.
-    /// </summary>
     public float maxNeighbourDistance = 10.0f;
-
-    /// <summary>
-    /// The Distance At Which To Check Downwards For Cliff Detection.
-    /// </summary>
     public float cliffSearchDistance = 1.5f;
 
-    /// <summary>
-    /// The List of Nodes in The PathGraph.
-    /// </summary>
     public List<Node> graph;
 
     [Header("Debug")]
-
-    /// <summary>
-    /// Detemines If Debug Visuals Would Be Shown For The PathGraph.
-    /// </summary>
     public bool showGraphGizmos;
 
+    // Get Setters
     public int MaxSize
     {
         get { return graphNodeContainer.childCount; }
     }
 
     #region Unity Functions
-
-    /// <summary>
-    /// Unitys Start Function.
-    /// </summary>
     private void Start()
     {
         CreateContainer();
     }
 
-    /// <summary>
-    /// Unitys Update Function.
-    /// </summary>
     private void Update()
     {
 
     }
 
-    /// <summary>
-    /// Unitys OnDrawGizmos Function.
-    /// </summary>
     private void OnDrawGizmos()
     {
         if (graph != null && showGraphGizmos)
@@ -87,31 +61,26 @@ public class PathGraph : MonoBehaviour, NodeContainer
         int numNodes = graphNodeContainer.childCount;
         graph = new List<Node>(numNodes);
 
-        foreach (Transform child in graphNodeContainer) // Iterates Through all Nodes GameObjects And Adds Them To The List.
+        foreach (Transform child in graphNodeContainer)
         {
-            graph.Add(new Node(true, child.position, 0, new List<Node>()));
+            graph.Add(new Node(true, child.position, new List<Node>()));
             child.gameObject.SetActive(false);
         }
 
-        foreach (Node node in graph) // Assigns All The Neighbour Nodes of The Nodes.
+        foreach (Node node in graph)
         {
             node.neighbours = GetNodeNeighbours(node);
         }
     }
 
-    public void CreateDebugVisuals()
-    {
-
-    }
-
     public Node GetNodeFromWorldPoint(Vector3 _worldPosition)
     {
-        Node closestNode = new Node(true, _worldPosition, 0, null);
+        Node closestNode = new Node(true, _worldPosition, null);
         float closestDistance = float.MaxValue;
 
         foreach (Node node in graph)
         {
-            float distance = Vector3.Distance(_worldPosition, node.worldPosition); // Checks The Distance To The Closes Node Position.
+            float distance = Vector3.Distance(_worldPosition, node.worldPosition);
             if (distance < closestDistance)
             {
                 closestDistance = distance;
@@ -129,15 +98,15 @@ public class PathGraph : MonoBehaviour, NodeContainer
         foreach (Node nodeToCompare in graph)
         {
             if (_node == nodeToCompare) continue;
-            float distance = Vector3.Distance(_node.worldPosition, nodeToCompare.worldPosition); // Checks The Distance Of The Node From The Current Node.
+            float distance = Vector3.Distance(_node.worldPosition, nodeToCompare.worldPosition);
 
             if (distance < maxNeighbourDistance)
             {
-                Vector3 edge = nodeToCompare.worldPosition - _node.worldPosition; // Vector To Iterate Across To Check For Edges.
+                Vector3 edge = nodeToCompare.worldPosition - _node.worldPosition;
 
                 bool cliff = false;
                 int steps = Mathf.FloorToInt(edge.magnitude);
-                for (int i = 0; i < steps; i++) // Steps Across The Direction of The Other Node, and Checks For Cliffs.
+                for (int i = 0; i < steps; i++)
                 {
                     Vector3 pos = _node.worldPosition + (edge.normalized * i);
 
@@ -148,7 +117,6 @@ public class PathGraph : MonoBehaviour, NodeContainer
                     }
                 }
 
-                // Final Check For Collisions In The Direction and Adds it To The List if No Cliff Was Found.
                 if (!Physics.Raycast(_node.worldPosition, edge, maxNeighbourDistance) && !cliff)
                 {
                     neighbours.Add(nodeToCompare);
@@ -157,11 +125,6 @@ public class PathGraph : MonoBehaviour, NodeContainer
         }
 
         return neighbours;
-    }
-
-    public void EnablePathingVisuals(bool isEnabled)
-    {
-
     }
     #endregion
 }
